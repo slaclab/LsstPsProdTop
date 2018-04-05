dae08e8cb3014cff49ae082bd14ce5762f6675c6
Larry Ruckman <ruckman@slac.stanford.edu>
2018-04-05 08:37:41 -0700

    Update setup_template.sh

--- software/rogue/setup_template.sh
+++ software/rogue/setup_template.sh
@@ -1,6 +1,7 @@
 # Setup environment
-source /afs/slac/g/reseng/rogue/v2.6.0/setup_env.sh
+source /afs/slac/g/reseng/rogue/v2.8.0/setup_env.sh
 #source /afs/slac/g/reseng/rogue/master/setup_env.sh
+#source /afs/slac/g/reseng/rogue/pre-release/setup_env.sh
 
 # Submodule Python Package directories
 export SURF_DIR=${PWD}/../../firmware/submodules/surf/python
